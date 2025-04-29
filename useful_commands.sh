5bf436428d874ae3bee980c096444ed8dd37306a
Bacon274 <jacob.fintan.mitchell.274@gmail.com>
2025-04-29 16:43:34 +0100

    save changes

--- useful_commands.sh
+++ useful_commands.sh
@@ -4,6 +4,5 @@ docker exec vibrationmonitoring-kapacitor-1 kapacitor define etl_task -type stre
 # Enable tasks
 docker exec vibrationmonitoring-kapacitor-1 kapacitor enable etl_task
 # for testing api on data
-curl -X POST http://localhost:8000/analyze   -H "Content-Type: application/json"   -d '{"start_time": "2025-04-24T15:45:42Z", "end_time": "2025-04-24T15:45:43Z"}'
-
+curl -X POST http://localhost:8000/analyze   -H "Content-Type: application/json"   -d '{"start_time": "2025-04-24T15:45:42Z", "end_time": "2025-04-24T17:45:43Z"}'
 curl -X POST http://localhost:8000/train   -H "Content-Type: application/json"   -d '{"start_time": "2025-04-24T15:45:42Z", "end_time": "2025-04-24T15:45:43Z"}'
